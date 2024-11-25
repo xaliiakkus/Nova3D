e8fbe5faa675e69adfd0b91534e0ae9b8290273f
Ali Akkuş <xaliakkus@gmail.com>
2024-11-25 23:04:05 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry
+          Welcome to Nova3D
         </p>
 
         <AnimatedTitle
